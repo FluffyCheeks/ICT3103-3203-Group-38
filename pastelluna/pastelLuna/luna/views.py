ffd7e5077b72898908212564bf00202239d9bfe8
Hui <73220938+TanYuHui@users.noreply.github.com>
2022-10-06 15:59:49 +0800

    retrieve and update

--- pastelluna/pastelLuna/luna/views.py
+++ pastelluna/pastelLuna/luna/views.py
@@ -1,5 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import *
+
 
 def home(request):
     """View function for home page of site."""
@@ -26,11 +30,26 @@ def home(request):
 
     # Render the HTML template index.html with the data in the context variable
     # return render(request, 'home.html', context=context)
-    #return render(request, 'home.html')
+    # return render(request, 'home.html')
     return render(request, 'home.html')
 
+
+@csrf_exempt
 def profile(request):
-    # inner join with id where user id =1
+    # inner join with id where user id =1 (pass in through param)
     obj = Users.objects.select_related("role_id").filter(id=1)
-    context = {"object": obj}
-    return render(request, "profile.html", context)
+
+    if request.method == 'POST':
+        if request.POST.get('save', '') == 'update':
+            editProfile = Users.objects.get(id=1)
+            editProfile.first_name = request.POST.get('firstname')
+            editProfile.last_name = request.POST.get('lastname')
+            editProfile.phone = request.POST.get('mobile')
+            editProfile.address = request.POST.get("address")
+            editProfile.allergies = request.POST.get("allergies")
+            # Save to the database here
+            editProfile.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        context = {"object": obj}
+        return render(request, "profile.html", context)
