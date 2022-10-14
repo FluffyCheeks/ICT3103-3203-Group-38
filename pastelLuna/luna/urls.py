b4688f7ac8d67803ae131f2079cebfcbf8d09410
asdwty <75032868+asdwty@users.noreply.github.com>
2022-10-14 19:13:33 +0800

    update to my branch
    latest one

---
+++ pastelLuna/luna/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+     path("home", views.home, name="home"),
+     path("profile", views.profile, name="profile"),
+     path('products', views.retrieve_product, name="products"),
+     path("registration",views.registration, name="registation"), #added this
+     path("shop", views.shop, name="shop"),
+     path('products/<int:pk>', views.retrieve_product_details,name="products_details"),
+]
+
