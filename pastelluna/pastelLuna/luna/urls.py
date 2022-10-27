027a9da149416a6e50b72219ad1303968b7a9bb3
2001189 <2001189@sit.singaportech.edu.sg>
2022-10-27 22:21:01 +0800

    Login with session
    Can login with session but password without hashing

--- pastelluna/pastelLuna/luna/urls.py
+++ pastelluna/pastelLuna/luna/urls.py
@@ -1,10 +1,28 @@
 from django.urls import path
-from . import views
+
+from .views.profile import profile
+from .views.product_details import retrieve_product_details
+from .views.home import shop, home
+from .views.user import editor_dash
+from .views.registration import registration
+from .views.admin_dashboard import admin_dashboard
+from .views.loginpage import loginpage
+from .views.logoutpage import logoutpage
+
 
 urlpatterns = [
-     path("", views.home, name="home"),
-     path("home", views.home, name="home"),
-     path("profile", views.profile, name="profile"),
-     path("login", views.loginpage, name="login"),
-     path("logout", views.logoutpage, name="home"),
+
+     path("", home, name="home"),
+     path("home", home, name="home"),
+     path("profile", profile, name="profile"),
+     path("registration",registration, name="registation"), #added this
+     path("loginpage", loginpage, name="loginpage"),
+     path("logout", logoutpage, name="home"),
+     path("shop", shop, name="shop"),
+     path('editor_dash', editor_dash, name="editor_dashboard"),
+     path('admin_dashboard', admin_dashboard, name="admin_dashboard"),
+     path('<slug:slug>', retrieve_product_details,name="product_details"),
+
+
 ]
+
