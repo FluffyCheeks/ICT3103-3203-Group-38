3442bc9f37d61b291eb715d8ba0e4f94135ac783
Hui <73220938+TanYuHui@users.noreply.github.com>
2022-11-04 04:02:56 +0800

    revert settings.py

--- pastelLuna/pastelLuna/pastelLuna/settings.py
+++ pastelLuna/pastelLuna/pastelLuna/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rm@2^(9bc++&yfa@@0f@91xvmg2aoe@5&n45rqek%kh!i28h(j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = []
 
 MESSAGE_TAGS = {
         messages.DEBUG: 'alert-secondary',
@@ -138,18 +138,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
+
+
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
+#MEDIA_URL = "/img/"
+
+MEDIA_URL = '/static/img/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
-
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
-MEDIA_URL = "/img/"
-
-# MEDIA_URL = '/static/img/'
-# STATIC_URL = '/static/'
-# MEDIA_ROOT = BASE_DIR / "static/img"
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
-# STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
+MEDIA_ROOT = BASE_DIR / "static/img"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
