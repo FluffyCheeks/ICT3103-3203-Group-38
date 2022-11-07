73add3371e8a096d676e6f2f8fe566829cb93de9
SynRein <77711928+SynRein@users.noreply.github.com>
2022-11-07 23:14:15 +0800

    Update product_details.py

--- pastelLuna/pastelLuna/luna/views/product_details.py
+++ pastelLuna/pastelLuna/luna/views/product_details.py
@@ -24,18 +24,15 @@ def check_for_cookie_session(request):
 def retrieve_product_details(request, slug):
     if check_for_cookie_session(request) != 2 or check_for_cookie_session(request) != 3:
         product_Detail = get_object_or_404(Product_Details,slug=slug)
-        print(product_Detail.id, "FROM DB")
-        num_cart = showcart_base(request) 
-        print(request.session['id'])
-        context = {"product": product_Detail,  'products_num': num_cart}
-        if request.method == 'POST':
-            if check_for_cookie_session(request):
+        if request.method == 'POST': 
+            while check_for_cookie_session(request):
                 uid = escape(request.session['id'])
                 user = Users.objects.get(id=uid)
                 if user is not None:
                     if request.POST.get('add_to_cart', '') == 'product_add':
                         product_id = int(request.POST.get('id'))
                         print(product_id, "-- FROM TEMP")
+                        print(product_Detail.id, "FROM DB")
                         product_price = escape(request.POST.get('price'))
                         if product_Detail.id == product_id:
                             product_status_qty = product_Detail.stock_available
@@ -53,6 +50,7 @@ def retrieve_product_details(request, slug):
                                 if product_status_qty >= product_qty:
                                     Cart.objects.create(user_id=user, product_id=product_Detail, quantity=product_qty, total_price=product_price)
                                     messages.success(request, 'Successfully added into cart')
+                                    print("cry more")
                                     return redirect("shop")
                                 else:
                                     messages.error(request,'Failed to add into cart; Out of Stock!')
@@ -62,14 +60,13 @@ def retrieve_product_details(request, slug):
                             return redirect("/")
                 else:
                     return redirect("loginpage")
-            else:
-                return redirect("loginpage")
-            # return redirect("/", slug=slug)
+            return redirect("loginpage")
         elif request.method == 'GET':
+            # while check_for_cookie_session(request):
+            num_cart = showcart_base(request) 
+            context = {"product": product_Detail,  'products_num': num_cart}
             return render(request,"product_details.html", context)
-        else:
-            messages.error(request,'Failed to add into cart; Try Again Later')
-            return redirect("/luna/product_details")
+            # return redirect("loginpage")
     else:
         return render(request, "unauthorised_user.html")
 
@@ -78,15 +75,10 @@ def retrieve_product_details(request, slug):
 def showcart_base(request):
     # needs to add into session
     check_for_cookie_session(request)
-    if check_for_cookie_session(request) == 1:
+    if int(check_for_cookie_session(request)) == 1:
         uid = escape(request.session['id'])
-        print("")
         num_of_prod = Cart.objects.filter(user_id=uid)
-        print(num_of_prod.count, "---- COUNTR")
         return num_of_prod.count
     else:
         num_of_prod = 0
-        uid = escape(request.session['id'])
-        print("uid ----", uid)
-        print(num_of_prod, "---- IN 0")
         return num_of_prod
